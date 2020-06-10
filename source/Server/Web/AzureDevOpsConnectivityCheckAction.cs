21a4b1acefc28cce1754ef95d78521afa19f514f
Shannon Lewis <slewis74@gmail.com>
2020-06-10 18:22:11 +1000

    Converted to using wrappers for responder actions

--- source/Server/Web/AzureDevOpsConnectivityCheckAction.cs
+++ source/Server/Web/AzureDevOpsConnectivityCheckAction.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Octopus.Data.Model;
 using Octopus.Server.Extensibility.Extensions.Infrastructure.Web.Api;
@@ -23,20 +21,19 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.Web
             this.adoApiClient = adoApiClient;
         }
 
-        public async Task ExecuteAsync(OctoContext context)
+        public async Task<OctoResponse> ExecuteAsync(IOctoRequest request)
         {
             var connectivityCheckResponse = new ConnectivityCheckResponse();
 
             try
             {
-                var json = await new StreamReader(context.Request.Body).ReadToEndAsync();
-                var request = JsonConvert.DeserializeObject<JObject>(json);
+                var requestData = request.GetBody<JObject>();
 
-                var baseUrl = request.GetValue("BaseUrl").ToString();
+                var baseUrl = requestData.GetValue("BaseUrl").ToString();
                 // If PersonalAccessToken here is null, it could be that they're clicking the test connectivity button after saving
                 // the configuration as we won't have the value of the PersonalAccessToken on client side, so we need to retrieve it
                 // from the database
-                var personalAccessToken = request.GetValue("PersonalAccessToken").ToString().ToSensitiveString();
+                var personalAccessToken = requestData.GetValue("PersonalAccessToken").ToString().ToSensitiveString();
                 if (string.IsNullOrEmpty(personalAccessToken.Value))
                 {
                     personalAccessToken = configurationStore.GetPersonalAccessToken();
@@ -45,8 +42,7 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.Web
                 if (string.IsNullOrEmpty(baseUrl))
                 {
                     connectivityCheckResponse.AddMessage(ConnectivityCheckMessageCategory.Error, "Please provide a value for Azure DevOps Base Url.");
-                    context.Response.AsOctopusJson(connectivityCheckResponse);
-                    return;
+                    return new OctoDataResponse(connectivityCheckResponse);
                 }
 
                 var urls = AdoProjectUrls.ParseOrganizationAndProjectUrls(baseUrl);
@@ -61,15 +57,13 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.Web
                     if (!projects.Succeeded)
                     {
                         connectivityCheckResponse.AddMessage(ConnectivityCheckMessageCategory.Error, projects.FailureReason);
-                        context.Response.AsOctopusJson(connectivityCheckResponse);
-                        return;
+                        return new OctoDataResponse(connectivityCheckResponse);
                     }
 
                     if (!projects.Value.Any())
                     {
                         connectivityCheckResponse.AddMessage(ConnectivityCheckMessageCategory.Error, "Successfully connected, but unable to find any projects to test permissions.");
-                        context.Response.AsOctopusJson(connectivityCheckResponse);
-                        return;
+                        return new OctoDataResponse(connectivityCheckResponse);
                     }
 
                     projectUrls = projects.Value.Select(project => new AdoProjectUrls
@@ -101,19 +95,17 @@ namespace Octopus.Server.Extensibility.IssueTracker.AzureDevOps.Web
                     if (!configurationStore.GetIsEnabled())
                     {
                         connectivityCheckResponse.AddMessage(ConnectivityCheckMessageCategory.Info, "The Jira Issue Tracker is not enabled, so its functionality will not currently be available");
-                        return;
                     }
 
-                    context.Response.AsOctopusJson(connectivityCheckResponse);
-                    return;
+                    return new OctoDataResponse(connectivityCheckResponse);
                 }
 
-                context.Response.AsOctopusJson(connectivityCheckResponse);
+                return new OctoDataResponse(connectivityCheckResponse);
             }
             catch (Exception ex)
             {
                 connectivityCheckResponse.AddMessage(ConnectivityCheckMessageCategory.Error, ex.ToString());
-                context.Response.AsOctopusJson(connectivityCheckResponse);
+                return new OctoDataResponse(connectivityCheckResponse);
             }
         }
     }
